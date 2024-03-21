e8b539fc89e7700ac38ab31dd94ed0c3db8b46da
Felipe Sousa <85742339+FelpSousaS@users.noreply.github.com>
2024-03-21 18:06:38 -0300

    classe Portaria criada

--- Portaria/src/main/kotlin/Console.kt
+++ Portaria/src/main/kotlin/Console.kt
@@ -1,14 +1,42 @@
 package org.example
 
-class Console {
-    fun readInt(msg: String): Int{
-        print(msg)
-        val info = readLine()
+class Console private constructor() {
+    companion object{
+        fun readInt(msg: String): Int {
+            var retorno: Int? = null
+            do{
+                print(msg)
+                val info = readLine()
 
-        if(info != null && info != ""){
-            return info.toInt()
+                if(info != null && info != ""){
+                    retorno = info.toIntOrNull()
+
+                    if(retorno == null || retorno <= 0){
+                        println("Valor inválido!")
+                    }
+                }else {
+                    println("Valor inválido!")
+                }
+            }while(retorno == null || retorno <= 0)
+
+            return retorno
+
         }
 
-        return 0
+        fun readString(msg: String): String{
+            var retorno: String? = null
+            do{
+                print(msg)
+                val info = readLine()
+
+                if(info != null && info != ""){
+                    retorno = info.lowercase()
+                }else{
+                    println("Valor inválido!")
+                }
+            }while(retorno == null)
+
+            return retorno
+        }
     }
 }
